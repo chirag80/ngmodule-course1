f4ff924d3a6812596eda9f81b23a38d8fd6a4bf3
ORION\ChiragPA <chiragpa@orioninc.com>
2017-07-04 18:54:59 -0400

    set port from env variable for Heroku deploy

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
+app.start = function () {
+  var port = process.env.PORT || 3000;
   // start the web server
-  return app.listen(function() {
+  return app.listen(port, function () {
+
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -20,10 +22,10 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
   if (require.main === module)
-    app.start();
+    app.start()
 });
